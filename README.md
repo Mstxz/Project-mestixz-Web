e99b93fd6716eefdfbe7dc9325b96b5584c105bd
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-12-14 01:37:15 +0700

    Add more page 'Moonfall'

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Mestixz official website
 
-## The announcement website of **Mestixz's World: the Project**'s Games
+This is the showcase of "Project Mestixz" game Series
+
+## Tehnologies Used in Website
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
+
+## For Editing
+
+TailwindCSS activate
+
+```nodejs
+npx tailwindcss -i .\src\input.css -o .\src\output.css --watch
+```
--- src/Script/parallax.js
+++ src/Script/parallax.js
@@ -1,14 +1,21 @@
 window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY;
-    const section3 = document.getElementById('games'); // Assuming you have an element with id "section-3"
-    
-    // Get the top offset of Section 3
-    const section3Top = section3.offsetTop;
 
-    // Change background if scroll position is greater than or equal to the top of Section 3
-    if (scrollPosition >= section3Top) {
-        document.body.style.backgroundImage = 'linear-gradient(rgba(10, 10, 10, 0.2), rgba(10, 10, 10, 1)), url("https://i.pinimg.com/1200x/41/5b/9c/415b9c7f0c3eec0b328b53be94b1fc6c.jpg")';
-    } else {
-        document.body.style.backgroundImage = 'linear-gradient(rgba(10, 10, 10, 0.2), rgba(10, 10, 10, 1)), url("https://i.pinimg.com/originals/dc/03/d6/dc03d6e820f9ade4dd39a32a36fa23ba.jpg")';
+    // Get all sections on the page
+    const sections = document.querySelectorAll('section');
+
+    // Check if there are at least 3 sections
+    if (sections.length >= 3) {
+        const section3 = sections[2]; // Third section (index starts at 0)
+        const section3Top = section3.offsetTop;
+
+        console.log(`Scroll Position: ${scrollPosition}, Section 3 Top: ${section3Top}`);
+
+        // Change background if scroll position is greater than or equal to the top of Section 3
+        if (scrollPosition >= section3Top) {
+            document.body.style.backgroundImage = 'linear-gradient(rgba(10, 10, 10, 0.2), rgba(10, 10, 10, 1)), url("https://i.pinimg.com/1200x/41/5b/9c/415b9c7f0c3eec0b328b53be94b1fc6c.jpg")';
+        } else {
+            document.body.style.backgroundImage = 'linear-gradient(rgba(10, 10, 10, 0.2), rgba(10, 10, 10, 1)), url("https://i.pinimg.com/1200x/1c/53/d3/1c53d3383cc64042e3308d6abba7753b.jpg")';
+        }
     }
 });
